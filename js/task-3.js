e7422f5624acfccc9ff776f6efa41709c0d64762
Solovitska <146887721+Solovitska@users.noreply.github.com>
2023-12-06 19:46:49 +0200

    up task

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,12 @@
 'use strict';
-function getShippingMessage(country, price, deliveryFee) {
-    //Розрахунок загальної вартості
-    const totalPrice = price + deliveryFee;
-    // Повідомлення про доставку
-    const message = `Shipping to ${country} will cost ${totalPrice} credits`;
-    // Повернення повідомлення
-    return message;
+function getElementWidth(content, padding, border) {
+    //Перетворюємо значення параметрів
+const contentWidth = Number.parseFloat(content);
+const paddingWidth = Number.parseFloat(padding);
+const borderWidth = Number.parseFloat(border);
+return contentWidth + paddingWidth * 2 + borderWidth * 2;
 }
 // Перевірка
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20));    // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20));    // "Shipping to Sweden will cost 120 credits"
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
